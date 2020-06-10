f6a6050c74c606c4bfd2afe41cadcbf11a179a6f
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-10 19:44:32 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,14 @@
 let state;
+
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  
+}
